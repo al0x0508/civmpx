4ab1f0c80167f1c2733f729a37c872d5ae160ed0
al0x0508 <alain.wackenheim@protonmail.com>
2018-07-25 18:46:41 +0200

    add vm creation

--- civmpx.sh
+++ civmpx.sh
@@ -5,6 +5,11 @@ WARN=$(echo -e "[\033[33m*\033[0m]")
 SUCCESS=$(echo -e "[\033[32m*\033[0m]")
 INFO=$(echo -e "[*]")
 
+function error_exit {
+    echo "$1" >&2   ## Send message to stderr. Exclude >&2 if you don't want it that way.
+    exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
+}
+
 function help(){
     echo "civmpx - Create VM from cloud images on Proxmox";
     echo "Usage example:";
@@ -47,7 +52,6 @@ while getopts ":i:p:" option; do
     esac
 done
 
-apt-get update -qq 2> /dev/null
 # Check if cloud-init is installed
 echo "$INFO Check if cloud-init is installed"
 if [ ! -x "$(command -v cloud-init)" ]; then
@@ -55,9 +59,14 @@ if [ ! -x "$(command -v cloud-init)" ]; then
     apt-get update -qq 2> /dev/null
     apt install -qq -y cloud-init 2> /dev/null
     if [ ! -x "$(command -v cloud-init)" ]; then
-        
+      echo "$ERROR Failed to install cloud-init. Aborting.."
+      exit 1
     fi
 else
     echo "$SUCCESS Ok"
 fi
+
+# Create VM
+echo "$INFO Creating VM"
+qm create $vmid && echo "$SUCCESS Ok" || error_exit "Failed to create VM"
 
